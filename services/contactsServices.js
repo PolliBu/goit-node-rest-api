9b50a07fd6cd8cefa7735e760e5ea4195c01c786
PolliBu <8polli.bu@gmail.com>
2024-03-29 14:30:15 +0200

    task 1-2

---
+++ services/contactsServices.js
@@ -0,0 +1,51 @@
+import { promises as fs } from "fs";
+import { nanoid } from "nanoid";
+import path from "path";
+
+const contactsPath = path.join("db", "contacts.json");
+
+export async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
+
+export async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) return null;
+    const deleteContact = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return deleteContact[0];
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: nanoid(), name, email, phone };
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
