ac5b3cbc76918fdffc40594c51d02714cc75bc19
PolliBu <8polli.bu@gmail.com>
2024-04-21 17:24:31 +0300

    Jimp2

--- helpers/avatarJimp.js
+++ helpers/avatarJimp.js
@@ -3,9 +3,9 @@ import Jimp from "jimp";
 export const avatarJimp = async (imagePath) => {
   try {
     const image = await Jimp.read(imagePath);
-    await image.resize(250, 250).quality(60).writeAsync(imagePath);
+    await image.resize(250, 250).quality(120).writeAsync(imagePath);
   } catch (err) {
     console.error(err);
-    throw new Error("");
+    throw new Error("Bad avatar");
   }
 };
